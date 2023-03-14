ba42b20789659fccad3e10059dc110f49aab1018
Suman Malakar <malakars041@gmail.com>
2023-03-14 23:58:20 +0530

    changing in navbar.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
+import Navbar from '../components/Navbar'
 import '../styles/globals.css'
-import Navbar from '../components/Navbar.js'
 
 export default function App({ Component, pageProps }) {
   return(
   <>
   <Navbar />
   <Component {...pageProps} />
-  </>)
+  </>
+  )
 }
