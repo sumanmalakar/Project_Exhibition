845483b64b60a95933217bb8d8cee77a97a37782
Suman Malakar <malakars041@gmail.com>
2023-03-14 23:16:47 +0530

    fixing newbar

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -2,19 +2,20 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <>
+    
     <nav>
       <ul className="flex justify-center bg-gray-500">
         <li className='p-3 bg-gray-900 text-white '>Home</li>
         <li className='p-3 bg-gray-900 text-white '>About</li>
         <li className='p-3 bg-gray-900 text-white '>Login</li>
+        <li className='p-3 bg-gray-900 text-white '>SingUp</li>
         <li className='p-3 bg-gray-900 text-white '>Contact</li>
         <li className='p-3 bg-gray-900 text-white '>Menu</li>
         <li className='p-3 bg-gray-900 text-white '>Teams</li>
       </ul>
     
     </nav>
-    <br />
-    </>
+  
+ 
   )
 }
