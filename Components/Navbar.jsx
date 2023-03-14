82491f686996ecfbd7e985e3a33120b6e533b824
Suman Malakar <malakars041@gmail.com>
2023-03-14 23:07:37 +0530

    adding login page

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -7,6 +7,7 @@ export default function Navbar() {
       <ul className="flex justify-center bg-gray-500">
         <li className='p-3 bg-gray-900 text-white '>Home</li>
         <li className='p-3 bg-gray-900 text-white '>About</li>
+        <li className='p-3 bg-gray-900 text-white '>Login</li>
         <li className='p-3 bg-gray-900 text-white '>Contact</li>
         <li className='p-3 bg-gray-900 text-white '>Menu</li>
         <li className='p-3 bg-gray-900 text-white '>Teams</li>
