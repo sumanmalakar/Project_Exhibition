f0383495bae8941161a2604ff6000aab9b08430c
Suman Malakar <malakars041@gmail.com>
2023-03-14 23:38:15 +0530

    changing the file name

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-import Navbar from '../Components/Navbar'
+import Navbar from '../components/Navbar'
 
 export default function App({ Component, pageProps }) {
   return(
