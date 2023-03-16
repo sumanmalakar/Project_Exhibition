9b93d65b76796cf02a104f802adafa68b2d6f297
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-16 18:58:45 +0530

    hover modification in navbar

--- components/nav.js
+++ components/nav.js
@@ -3,14 +3,14 @@ import React from 'react'
 export default function Nav() {
   return (
     <>
-      <ul className="flex justify-center bg-gray-500">
-        <li className='p-3 bg-gray-900 text-white '>Home</li>
-        <li className='p-3 bg-gray-900 text-white '>About</li>
-        <li className='p-3 bg-gray-900 text-white '>Login</li>
-        <li className='p-3 bg-gray-900 text-white '>SingUp</li>
-        <li className='p-3 bg-gray-900 text-white '>Contact</li>
-        <li className='p-3 bg-gray-900 text-white '>Menu</li>
-        <li className='p-3 bg-gray-900 text-white '>Teams</li>
+      <ul className="flex justify-center bg-gray-900">
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Home</li>
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>About</li>
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Login</li>
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Sing-Up</li>
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Contact</li>
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Menu</li>
+        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Teams</li>
       </ul>
 
     </>
