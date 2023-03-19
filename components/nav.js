99bb45d20c5204c998b355c84979c99696ab03b7
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-19 23:46:40 +0530

    added icons in navbar and added slider file

--- components/nav.js
+++ components/nav.js
@@ -1,17 +1,34 @@
 import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { library } from '@fortawesome/fontawesome-svg-core'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { ImSearch } from "react-icons/im";
+import { MdShoppingCart } from "react-icons/md";
+
+// import { ImSearch } from "react-icons";
 
 export default function Nav() {
   return (
     <>
-      <ul className="flex justify-center bg-gray-900">
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Home</li>
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>About</li>
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Login</li>
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Sing-Up</li>
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Contact</li>
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Menu</li>
-        <li className='p-3 bg-gray-900 text-white hover:bg-white hover:text-black hover:cursor-pointer'>Teams</li>
+    
+      <div className=' flex justify-center bg-gray-500'>
+      <ul className=" flex justify-center ">
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Home</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>About</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Login</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Sing-Up</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Contact</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Menu</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Teams</li>
       </ul>
+      {/* <ImSearch/> */}
+      <div className="icon pl-20 pt-4 flex ">
+      <a href="" className='hover:cursor-pointer hover:scale-150 pl-4'><ImSearch/></a>
+      <a href="" className='hover:cursor-pointer hover:scale-150 pl-4'><MdShoppingCart/></a>
+
+      </div>
+      </div>
+
 
     </>
  
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import Navbar from '../components/nav'
+import Slider from '../components/slider'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return(
   <>
   <Navbar/>
+  <Slider/>
   <Component {...pageProps} />
   </>
   )
