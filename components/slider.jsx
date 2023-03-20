4008ff4e7d7c218496686845a3da7eda8da086a0
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-20 18:37:07 +0530

    added slider

--- components/slider.jsx
+++ components/slider.jsx
@@ -1,89 +1,27 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import AwesomeSlider from 'react-awesome-slider';
+import withAutoplay from 'react-awesome-slider/dist/autoplay';
+import 'react-awesome-slider/dist/styles.css';
+ 
+
 export default function slider() {
+  const AutoplaySlider = withAutoplay(AwesomeSlider);
+ const name = "vaishnavi"
+const slider = (
+  <AutoplaySlider
+    play={true}
+    cancelOnInteraction={false} // should stop playing on user interaction
+    interval={100}
+  >
+    <div data-src="https://i.gadgets360cdn.com/large/noise_colorfit_pro_4_review_main_1659433865849.jpg" />
+    <div data-src="https://5.imimg.com/data5/ANDROID/Default/2023/1/SH/SW/KI/135027494/product-jpeg-1000x1000.jpg" />
+    <div data-src="https://cdn.shopify.com/s/files/1/1603/9553/collections/beta_banner_1200x630.png?v=1670065562" />
+  </AutoplaySlider>
+);
   return (
-    // <div>slider</div>
-    <div
-    id="carouselExampleControls"
-    className="relative"
-    data-te-carousel-init
-    data-te-carousel-slide>
-    <div
-      className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-      <div
-        className="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-        data-te-carousel-item
-        data-te-carousel-active>
-        <img
-          src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-          className="block w-full"
-          alt="Wild Landscape" />
-      </div>
-      <div
-        className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-        data-te-carousel-item>
-        <img
-          src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-          className="block w-full"
-          alt="Camera" />
-      </div>
-      <div
-        className="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-        data-te-carousel-item>
-        <img
-          src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-          className="block w-full"
-          alt="Exotic Fruits" />
-      </div>
-    </div>
-    <button
-      className="absolute top-0 bottom-0 left-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-      type="button"
-      data-te-target="#carouselExampleControls"
-      data-te-slide="prev">
-      <span className="inline-block h-8 w-8">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-6 w-6">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5" />
-        </svg>
-      </span>
-      <span
-        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-        >Previous</span
-      >
-    </button>
-    <button
-      className="absolute top-0 bottom-0 right-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-      type="button"
-      data-te-target="#carouselExampleControls"
-      data-te-slide="next">
-      <span className="inline-block h-8 w-8">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="h-6 w-6">
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-        </svg>
-      </span>
-      <span
-        className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-        >Next</span
-      >
-    </button>
-  </div>
+    <>
+    {slider}
+    </>
+
   )
 }
