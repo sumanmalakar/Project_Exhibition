c89444ff4aa4f7415f63e7ce867fe1dbfb29d6c5
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-20 16:20:30 +0530

    navbar modification

--- components/nav.jsx
+++ components/nav.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/jsx-no-undef */
+
 import React from 'react'
 import Link from 'next/link'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -7,7 +7,7 @@ import Link from 'next/link'
 import { ImSearch } from "react-icons/im";
 import { MdShoppingCart } from "react-icons/md";
 
-// import { ImSearch } from "react-icons";
+
 
 export default function Nav() {
   return (
@@ -23,11 +23,10 @@ export default function Nav() {
         </li>
         <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Login</li>
         <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Sing-Up</li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Contact</li>
+        {/* <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Contact</li>
         <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Menu</li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Teams</li>
+        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Teams</li> */}
       </ul>
-      {/* <ImSearch/> */}
       <div className="icon pl-20 pt-4 flex ">
       <Link href="" className='hover:cursor-pointer hover:scale-150 pl-4'><ImSearch/></Link>
       <Link href="" className='hover:cursor-pointer hover:scale-150 pl-4'><MdShoppingCart/></Link>
