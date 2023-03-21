964974910060dc3e847cd0f75d9b93f769574f38
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-21 18:04:30 +0530

    created smartphone page

--- pages/smartphone.jsx
+++ pages/smartphone.jsx
@@ -1,7 +1,103 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react'
 
 export default function smartphone() {
   return (
-    <div>smartphone</div>
+    // <div>smartphone</div>
+    <div className="box flex lg:flex-row justify-center mt-20 md:flex-col">
+    <div class="flex justify-center mr-8 ">
+  <div
+    class="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+    <a href="#!" data-te-ripple-init data-te-ripple-color="light">
+    <div className='flex justify-center'>
+      <img
+        class="rounded-t-lg h-[50vh]"
+        src="https://cdn.dxomark.com/wp-content/uploads/medias/post-132367/Apple-iPhone-14_Plus_FINAL_featured-image-packshot-review.jpg"
+        alt="" />
+        </div>
+    </a>
+    <div class="p-6">
+      <h5
+        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+        Card title
+      </h5>
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Some quick example text to build on the card title and make up the
+        bulk of the cards content.
+      </p>
+      <button
+        type="button"
+        class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+        data-te-ripple-init
+        data-te-ripple-color="light">
+        Button
+      </button>
+    </div>
+  </div>
+</div>
+
+<div class="flex justify-center mr-8">
+  <div
+    class="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+    <a href="#!" data-te-ripple-init data-te-ripple-color="light">
+        <div className='flex justify-center'>
+      <img
+        class="rounded-t-lg h-[50vh]"
+        src="https://image.oppo.com/content/dam/oppo/common/mkt/v2-2/reno8-t-5g-en/middlebanner/reno8-t-5g_640_480.jpg.thumb.webp"
+        alt="" />
+        </div>
+    </a>
+    <div class="p-6">
+      <h5
+        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-100">
+        Card title
+      </h5>
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Some quick example text to build on the card title and make up the
+        bulk of the cards content.
+      </p>
+      <button
+        type="button"
+        class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+        data-te-ripple-init
+        data-te-ripple-color="light">
+        Button
+      </button>
+    </div>
+  </div>
+</div>
+
+<div class="flex justify-center">
+  <div
+    class="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700">
+    <a href="#!" data-te-ripple-init data-te-ripple-color="light">
+    <div className='flex justify-center'>
+      <img
+        class="rounded-t-lg h-[50vh]"
+        src="https://www.notebookcheck.net/uploads/tx_nbc2/SamsungGalaxyA22__1_.jpg"
+        alt="" />
+        </div>
+    </a>
+    <div class="p-6">
+      <h5
+        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+        Card title
+      </h5>
+      <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+        Some quick example text to build on the card title and make up the
+        bulk of the cards content.
+      </p>
+      <button
+        type="button"
+        class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+        data-te-ripple-init
+        data-te-ripple-color="light">
+        Button
+      </button>
+    </div>
+  </div>
+</div>
+
+</div>
   )
 }
