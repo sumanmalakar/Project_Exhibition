8f56411d1546ed04ecc7236ed8759aade55d0d6e
Suman Malakar <malakars041@gmail.com>
2023-03-21 18:21:40 +0530

    changing the background color

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className='bg-black'>
+      <body className='bg-[#0d1c3b]'>
         <Main />
         <NextScript />
       </body>
