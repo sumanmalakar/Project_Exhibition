fe85da19a417702139c82929f9347780d8dd9485
Suman Malakar <malakars041@gmail.com>
2023-03-29 12:52:38 +0530

    updating navbar and creating login

--- components/nav.jsx
+++ components/nav.jsx
@@ -6,43 +6,49 @@ import Link from 'next/link'
 // import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { ImSearch } from "react-icons/im";
 import { MdShoppingCart } from "react-icons/md";
+import { MdAccountCircle } from "react-icons/md";
+
+
 
 
 
 export default function Nav() {
   return (
     <>
-    
-      <div className=' flex justify-center bg-gray-500'>
-      <ul className=" flex justify-center ">
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
-          <Link href="/">Home</Link>
-          
-          </li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
-        <Link href="/about">About </Link>
-        </li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Login</li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>Sing-Up</li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
-          <Link  href="/smartwatch">Smartwatches</Link>
-        </li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
-          <Link href="/smartphone">Smartphones</Link>
-        </li>
-        <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
-          <Link href="/bluetoothdevice">Bluetooth Devices</Link> 
-            </li>
-      </ul>
-      <div className="icon pl-20 pt-4 flex ">
-      <Link href="" className='hover:cursor-pointer hover:scale-150 pl-4'><ImSearch/></Link>
-      <Link href="" className='hover:cursor-pointer hover:scale-150 pl-4'><MdShoppingCart/></Link>
 
-      </div>
+      <div className='sticky top-0 flex flex-col md:flex-row justify-around bg-pink-400 w-[100%] z-10'>
+        <div >
+        <ul className="flex justify-center items-center">
+          <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
+            <Link href="/">Home</Link>
+
+          </li>
+          {/* <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
+            <Link href="/about">About </Link>
+          </li> */}
+          <Link href="/smartwatch">
+          <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
+          Smartwatches
+          </li></Link>
+          <Link href="/smartphone">
+          <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
+           Smartphones
+          </li></Link>
+          <Link href="/bluetoothdevice">
+          <li className='p-3  text-black hover:bg-black hover:text-white hover:cursor-pointer'>
+            Bluetooth Devices
+          </li></Link>
+        </ul>
+        </div>
+        <div className="icon  flex justify-center items-center ">
+          <Link href="" ><ImSearch className="text-3xl md:text-2xl mx-3 " /></Link>
+          <Link href=""><MdShoppingCart  className="text-3xl md:text-2xl mx-3 " /></Link>
+          <Link href="/login"><MdAccountCircle className="text-3xl md:text-2xl mx-3" /></Link>
+        </div>
       </div>
 
 
     </>
- 
+
   )
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="overflow-x-hidden">
       <Head />
       <body className='bg-[#0d1c3b]'>
         <Main />
