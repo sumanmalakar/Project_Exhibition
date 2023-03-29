2cf1b2c47ad587b5525318233798d21930d8cbd6
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-29 12:15:46 +0530

    created slug file

---
+++ pages/product/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+
+
+export default function Product_page() {
+ 
+  return (
+    <div className="text-white">This is over slug page  </div>
+  )
+}
