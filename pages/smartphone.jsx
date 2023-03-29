93f089cff33cd68cb67eceb36698745e7926fa97
vaishna1vi <vaishnavinagvanshi68675@gmail.com>
2023-03-29 15:15:08 +0530

    Modified footer mobile links and smartphone content

--- pages/smartphone.jsx
+++ pages/smartphone.jsx
@@ -18,18 +18,18 @@ export default function smartphone() {
     <div class="p-6">
       <h5
         class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-        Card title
+        iPhone 14 Pro
       </h5>
       <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-        Some quick example text to build on the card title and make up the
-        bulk of the cards content.
+      new iPhone 14 Pro when you trade in iPhone 11 or higher. Terms Apply.
+      All-new 48MP Main camera · New safety features · A16 Bionic · All-day battery life
       </p>
       <button
         type="button"
         class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
         data-te-ripple-init
         data-te-ripple-color="light">
-        Button
+       Rs. 95,000/-
       </button>
     </div>
   </div>
@@ -49,18 +49,17 @@ export default function smartphone() {
     <div class="p-6">
       <h5
         class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-100">
-        Card title
+        Oppo F1
       </h5>
       <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-        Some quick example text to build on the card title and make up the
-        bulk of the cards content.
+      The phone featured a 5 in (130 mm) touchscreen display, Android 5.1 (Lollipop) operating system, and microSD card support for up to an additional 256GB of storage
       </p>
       <button
         type="button"
         class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
         data-te-ripple-init
         data-te-ripple-color="light">
-        Button
+        Rs. 30,000/-
       </button>
     </div>
   </div>
@@ -80,18 +79,17 @@ export default function smartphone() {
     <div class="p-6">
       <h5
         class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-        Card title
+        Galaxy F-41
       </h5>
       <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-        Some quick example text to build on the card title and make up the
-        bulk of the cards content.
+      The phone has a 6000mAh battery, 64MP main camera, as well as an ultrawide and depth sensor, and a 6.4 in super AMOLED display.
       </p>
       <button
         type="button"
         class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
         data-te-ripple-init
         data-te-ripple-color="light">
-        Button
+        Rs. 20,000/-
       </button>
     </div>
   </div>
