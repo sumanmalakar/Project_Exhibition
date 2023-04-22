b9430cae368ba5a14bc7a543bca99ecc43562faa
Suman Malakar <malakars041@gmail.com>
2023-04-23 00:37:32 +0530

    adding pincode servies, updating the slug.js page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,14 +1,39 @@
 /* eslint-disable @next/next/no-img-element */
 import { useRouter } from "next/router";
-
-
+import { useState } from "react";
 // import { json } from "stream/consumers";
-
 
 const Slug = () => {
 
   const router = useRouter();
   const { slug } = router.query;
+
+  const [pin, setPin] = useState();
+  const [service, setService] = useState();
+
+  const checkServiceability = async (e) => {
+    e.preventDefault();
+    // let pins = await fetch('http://localhost:3000/api/pincode')
+
+    let pins = await fetch("/api/pincode");
+    let pinJson = await pins.json();
+
+    
+    console.log(pinJson, pin);
+
+    if (pinJson.includes(parseInt(pin))) {
+      setService(true);
+      console.log(service);
+    } else {
+      setService(false);
+      console.log(service);
+    }
+  };
+
+  const onChangePin = (e) => {
+    setPin(e.target.value);
+    console.log(pin);
+  };
 
 
   return (
@@ -87,41 +112,39 @@ const Slug = () => {
                 </button>
 
 
-                {/* <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                  <svg
-                    fill="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    class="w-5 h-5"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                  </svg>
-                </button> */}
+      
 
 
               </div>
 
-              {/* pincode */}
+            {/* pincode */}
 
-              <div className="pin mt-10 flex space-x-2 text-sm">
+            <div className="pin mt-10 flex space-x-2 text-sm">
                 <input
                   type="text"
                   placeholder="Enter pincode"
                   className=" px-2 border-2
                   text-black border-pink-400 sm:text-2xl"
                   // value={pin}
-                 
+                  onChange={onChangePin}
                 />
                 <button
-               
+                  onClick={checkServiceability}
                   className="flex ml-16 text-white bg-pink-500 border-0 py-2 px-6 focus:outline-none hover:bg-pink-600 rounded"
                 >
                   Check PinCode
                 </button>
               </div>
-            
+              {(!service && service != null) && (
+                <div className="text-red-700 text-sm mt-3">
+                  Sorry! We do not deliver to this pincode yet...
+                </div>
+              )}
+              {(service && service != null) && (
+                <div className="text-green-700 text-sm mt-3">
+                  Yay! This pincode is serviceable..
+                </div>
+              )}
             </div>
           </div>
         </div>
