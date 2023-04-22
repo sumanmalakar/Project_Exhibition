7feacb43d1dee5a60b280259e933e0ae09d14c55
Suman Malakar <malakars041@gmail.com>
2023-04-23 00:30:56 +0530

    start working on backend, creating pincode.js

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className="overflow-x-hidden">
       <Head />
-      <body >
-      {/* <body className='bg-[#0d1c3b]'> */}
+      <body className='bg-[#0d1c3b]'>
         <Main />
         <NextScript />
       </body>
